cb73a267c59c013567230ba3cfe3977852f61cc3
turbek <turbek93@gmail.com>
2017-02-28 00:55:47 +0100

    the prototype is working! yay

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,18 @@
  * Created by turbek on 27.02.17.
  */
 public class Main {
-    static String TARGET_STRING = "first try.";
-    static int MUTATION_RATE = 1;
-    static int POPULATION_MAX;
+    static String TARGET_STRING = "Balazs olyan okos!";
+    static double MUTATION_RATE = 0.01;
+    static int POPULATION_MAX = 200;
 
     public static void main(String[] args) {
         Population population = new Population(POPULATION_MAX, MUTATION_RATE, TARGET_STRING);
         population.generatePopulation();
-        while(true) {
+        while(!population.checkIfFinished()) {
             population.calculateFitness();
             population.matingPool();
-            population.chooseParents();
             population.generateNewPopulation();
         }
+        System.out.println("finished in: "+population.getGenerationCount()+" generations.");
     }
 }
