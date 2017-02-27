6c7883a87469d34ce7c6a46d8e525a713650bbb4
turbek <turbek93@gmail.com>
2017-02-27 21:37:55 +0100

    create a chromosome with an array of chars(gene)

---
+++ src/main/java/Chromosome.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by turbek on 27.02.17.
+ */
+public class Chromosome {
+    int length;
+    char[] genes;
+
+    Chromosome(int length){
+        this.length = length;
+        this.genes = generateChromosome();
+    }
+
+    private char generateGene(){
+        Random r = new Random();
+        int value = r.nextInt((122 - 32) + 1) + 32;
+        System.out.println(value);
+        char randomChar = (char)value;
+        return randomChar;
+    }
+
+    private char[] generateChromosome(){
+        char[] genes = new char[this.length];
+        for(int i = 0; i < this.length; i++){
+            genes[i] = generateGene();
+        }
+        return genes;
+    }
+
+    @Override
+    public String toString() {
+        return "Chromosome{" +
+                "genes=" + Arrays.toString(genes) +
+                '}';
+    }
+}
+
