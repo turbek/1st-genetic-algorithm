3d0ab8741bf268df2cf97866e4d6aae2ecd15a1f
turbek <turbek93@gmail.com>
2017-02-28 00:23:02 +0100

    population changed to array, unnecessary use of arraylist

--- src/main/java/Population.java
+++ src/main/java/Population.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
  * Created by turbek on 27.02.17.
  */
 public class Population {
-    private ArrayList<Chromosome> population = new ArrayList<>();
+    private Chromosome[] population;
+//    private ArrayList<Chromosome> population = new ArrayList<>();
     private ArrayList<Chromosome> matingPool;
     private int popMax;
     private double mutation;
@@ -15,19 +16,21 @@ public class Population {
         this.popMax = popMax;
         this.mutation = mutationRate.doubleValue()/100;
         this.target = target;
+        this.population = new Chromosome[popMax];
     }
 
     public void generatePopulation() {
         for(int i = 0; i < this.popMax; i++){
-            this.population.add(new Chromosome(target.length()));
+            this.population[i] = new Chromosome(target.length());
         }
     }
 
     public void calculateFitness() {
         for(int i = 0; i < this.popMax; i++) {
-            this.population.get(i).checkFitness(this.target);
-            if(this.population.get(i).fitness > this.maxFitness){
-                this.maxFitness = this.population.get(i).fitness;
+            this.population[i].checkFitness(this.target);
+            if(this.population[i].fitness > this.maxFitness){
+                this.maxFitness = this.population[i].fitness;
+                System.out.println("new maximum fitness: "+this.maxFitness);
             }
         }
     }
@@ -35,8 +38,29 @@ public class Population {
     public void matingPool() {
         this.matingPool = new ArrayList<>();
         for(int i = 0; i < this.popMax; i++) {
-
+            Float mateNumFloat = this.population[i].fitness / maxFitness * 100;
+            int mateNumInt = mateNumFloat.intValue();
+            for(int j = 0; j < mateNumInt; j++) {
+                this.matingPool.add(this.population[i]);
+            }
         }
+        System.out.println("mating pool size: "+this.matingPool.size());
+    }
+
+    public void generateNewPopulation() {
+//        for (int i = 0; i < this.population.length; i++){
+//            Chromosome parent1 = this.matingPool.get(new Random().nextInt(this.matingPool.size()+1));
+//            Chromosome parent2 = this.matingPool.get(new Random().nextInt(this.matingPool.size()+1));
+//            Chromosome child = parent1.crossover(parent2);
+//            child.mutate();
+//            this.population. = child;
+//        }
+
+//        parentA
+//        parentB
+//        crossover
+//        mutation
+//        addtopopulation
     }
 
     @Override
