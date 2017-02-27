7ad8363a6ad6ad258ee81a2d6fa18c1a1973a85c
turbek <turbek93@gmail.com>
2017-02-27 23:07:55 +0100

    now calculating fitness for population and checking maximum fitness

--- src/main/java/Population.java
+++ src/main/java/Population.java
@@ -5,20 +5,37 @@ import java.util.ArrayList;
  */
 public class Population {
     private ArrayList<Chromosome> population = new ArrayList<>();
+    private ArrayList<Chromosome> matingPool;
     private int popMax;
     private double mutation;
     private String target;
+    private float maxFitness = 0.0f;
 
     Population(int popMax, Integer mutationRate, String target){
         this.popMax = popMax;
         this.mutation = mutationRate.doubleValue()/100;
         this.target = target;
-        generatePopulation();
     }
 
-    private void generatePopulation() {
+    public void generatePopulation() {
         for(int i = 0; i < this.popMax; i++){
             this.population.add(new Chromosome(target.length()));
+        }
+    }
+
+    public void calculateFitness() {
+        for(int i = 0; i < this.popMax; i++) {
+            this.population.get(i).checkFitness(this.target);
+            if(this.population.get(i).fitness > this.maxFitness){
+                this.maxFitness = this.population.get(i).fitness;
+            }
+        }
+    }
+
+    public void matingPool() {
+        this.matingPool = new ArrayList<>();
+        for(int i = 0; i < this.popMax; i++) {
+
         }
     }
 
