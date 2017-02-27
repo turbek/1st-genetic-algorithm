64c2cdcbcd750b8a66e4b1d455c2506d69410e4b
turbek <turbek93@gmail.com>
2017-02-27 21:50:31 +0100

    main for some tests

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by turbek on 27.02.17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Chromosome chromosome = new Chromosome(10);
+        System.out.println(chromosome);
+    }
+}
