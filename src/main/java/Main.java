05adb7b805c6fa3d686fafa8c756a1788de9afc3
turbek <turbek93@gmail.com>
2017-02-27 23:08:13 +0100

    while loop for main added with some unfinished methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,18 @@
  * Created by turbek on 27.02.17.
  */
 public class Main {
+    static String TARGET_STRING = "first try.";
+    static int MUTATION_RATE = 1;
+    static int POPULATION_MAX;
 
     public static void main(String[] args) {
-        Population population = new Population(100, 1, "asdasd");
-        System.out.println(population.toString());
+        Population population = new Population(POPULATION_MAX, MUTATION_RATE, TARGET_STRING);
+        population.generatePopulation();
+        while(true) {
+            population.calculateFitness();
+            population.matingPool();
+            population.chooseParents();
+            population.generateNewPopulation();
+        }
     }
 }
