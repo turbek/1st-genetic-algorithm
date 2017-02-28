63e328ec397518bca3f085a79a98439453760c56
turbek <turbek93@gmail.com>
2017-02-28 01:22:10 +0100

    added some sout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@
  * Created by turbek on 27.02.17.
  */
 public class Main {
-    static String TARGET_STRING = "My first try!";
+    static String TARGET_STRING = "My first try and I'm so proud!";
     static double MUTATION_RATE = 0.02;
-    static int POPULATION_MAX = 200;
+    static int POPULATION_MAX = 1000;
 
     public static void main(String[] args) {
         Population population = new Population(POPULATION_MAX, MUTATION_RATE, TARGET_STRING);
@@ -13,6 +13,8 @@ public class Main {
             population.calculateFitness();
             population.matingPool();
             population.generateNewPopulation();
+            if (population.getGenerationCount() % 500 == 0)
+                System.out.println("generation count at: "+population.getGenerationCount());
         }
         System.out.println("finished in: "+population.getGenerationCount()+" generations.");
     }
