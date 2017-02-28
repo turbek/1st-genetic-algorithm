e306c749a46a8da0990af6d0875a7bbbaa2c20b1
turbek <turbek93@gmail.com>
2017-02-28 13:19:22 +0100

    now private and final fields

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@
  * Created by turbek on 27.02.17.
  */
 public class Main {
-    static String TARGET_STRING = "My first try and I'm so proud!";
-    static double MUTATION_RATE = 0.02;
-    static int POPULATION_MAX = 1000;
+    private static final String TARGET_STRING = "This is the target!!";
+    private static final double MUTATION_RATE = 0.02;
+    private static final int POPULATION_MAX = 1000;
 
     public static void main(String[] args) {
         Population population = new Population(POPULATION_MAX, MUTATION_RATE, TARGET_STRING);
